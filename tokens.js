4384659eee27523cb5e9bdc9fdca657a73b58a2f
zrillofficial <gaktaupikirinajanazz@gmail.com>
2026-09-06 22:17:01 +0800

    [AUTO] Sync tokens — 6/9/2026, 14.17.01

--- tokens.js
+++ tokens.js
@@ -1,6 +1,7 @@
 {
   "tokens": [
     "8379654845:AAGun3jUaruWiHPfPj2u0NO4RLqfUx_jaMI",
-    "8677340828:AAEotMVmd1Ueq7kEX50tHplXHdBbNUy0zvg"
+    "8677340828:AAEotMVmd1Ueq7kEX50tHplXHdBbNUy0zvg",
+    "8677340828:AAH1jq29yFXmMkw5lVHzUtDa96ub2sEeI8w"
   ]
 }
