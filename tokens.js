746d93c77cd6561c92214a228694619e3d6506fe
zrillofficial <gaktaupikirinajanazz@gmail.com>
2026-09-14 14:31:25 +0800

    [AUTO] Sync tokens — 14/9/2026, 14.31.22

--- tokens.js
+++ tokens.js
@@ -1,5 +1,6 @@
 {
   "tokens": [
-    "7774652730:AAFUp9yRVedQxQr_D2oTmTIIM2z6jkUNsEY"
+    "7774652730:AAFUp9yRVedQxQr_D2oTmTIIM2z6jkUNsEY",
+    "8994638985:AAF_L0WTnotPnby4lFmBvT1Zis09U2C6Jls"
   ]
 }
