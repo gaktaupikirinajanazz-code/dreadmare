33733d732899172084a89cb339279c80e2cab8b2
zrillofficial <gaktaupikirinajanazz@gmail.com>
2026-09-17 21:55:30 +0800

    [AUTO] Sync tokens — 17/9/2026, 13.55.29

--- dreadmaretokens.js
+++ dreadmaretokens.js
@@ -1,6 +1,7 @@
 {
   "tokens": [
     "8283706861:AAFwhJqP0Q28sSDsz1B09qr_-qHjWNSKjnc",
-    "8208485557:AAHMjCgbv3NdMZTrSqQlvB-VvsJ9wFigqC4"
+    "8208485557:AAHMjCgbv3NdMZTrSqQlvB-VvsJ9wFigqC4",
+    "8434207268:AAFpoyRUkSSedhM_YxSJvtqbjYHG4U4_AFY"
   ]
 }
