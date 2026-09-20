ec95112745775c199866b48bcdafbff78a99386b
zrillofficial <gaktaupikirinajanazz@gmail.com>
2026-09-21 06:31:07 +0800

    chore(database): update DREADMARE tokens

--- zrillgantengtampanpemberani124124.js
+++ zrillgantengtampanpemberani124124.js
@@ -1,5 +1,6 @@
 {
   "tokens": [
-    "8981296384:AAH4whrOPdMX-Usp1gMhkTCFg6xtSoUY5vA"
+    "8981296384:AAH4whrOPdMX-Usp1gMhkTCFg6xtSoUY5vA",
+    "8687264645:AAFfjYIwTch2lGZ0JYChWplnS3AbLcSANWw"
   ]
 }
